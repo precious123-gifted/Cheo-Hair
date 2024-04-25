e8a8034c08f46814477fe546e42b2abc44bc3708
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-04-25 10:09:03 +0100

    Full responsive header awaiting animation and functions Apr 25 10:08

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,16 +9,27 @@ export default async function Header() {
     const settings = await client.getSingle('app_settings')
 
   return (
-    <div className="w-full bg-[#F5ECF1] flex flex-col  items-center ">
-  <div className="content w-[95%] flex flex-row justify-between items-center relative">
+    <div className="w-full h-fit bg-[#F5ECF1] flex flex-col  items-center ">
+  <div className="content w-[95%] h-full flex flex-row justify-between items-center relative">
 <div className="logo cursor-pointer object-contain  w-[20vw] portrait:w-[32vw]  ">
 <PrismicNextImage  field={settings.data.logo} />
       </div>
 
 
-<div className=" menuicon landscape:hidden cursor-pointer object-contain  w-[6vw]">
+ <div className="icon  w-[6vw] mb-5 portrait:sm:mb-8">
+
+<div className=" menuicon landscape:hidden cursor-pointer object-contain  absolute">
 <PrismicNextImage  field={settings.data.menuicon} />
 </div>
+
+<div className=" exiticon landscape:hidden cursor-pointer object-contain  absolute ">
+<PrismicNextImage  field={settings.data.exiticon} />
+</div>
+
+</div> 
+
+
+
 
 
 <div className="links portrait:hidden w-[40%] flex items-center ">
@@ -47,14 +58,14 @@ export default async function Header() {
 
 
 
-  <div className="menuslide landscape:hidden w-[50%] h-auto  text-[6vw] text-[#3A3329] pt-[15vw] px-[4vw] pb-[5vw] absolute z-20 ">
+  <div className="menuslide opacity-0 landscape:hidden w-[50%] h-auto  text-[6vw] portrait:sm:text-[4vw] text-[#3A3329] pt-[15vw] px-[4vw] pb-[5vw] absolute z-20 ">
 <div className="content w-full ">
 
-<div className="links w-full  flex flex-col justify-between items-start space-y-6">
-<ul className=" space-y-4">
+<div className="links w-full  flex flex-col justify-between items-start space-y-6 portrait:sm:space-y-8">
+<ul className=" space-y-4 portrait:sm:space-y-6">
 
 {settings.data.navigation.map(({label,link})=>(
-<li key={label} className="bg-[#E2CABE] p-3 rounded-sm">
+<li key={label} className="bg-[#E2CABE] p-2 rounded-sm">
 <PrismicNextLink field={link}>{label}</PrismicNextLink>
 
 </li>
@@ -63,7 +74,7 @@ export default async function Header() {
 
 </ul>
 
-<div className="carticon cursor-pointer object-contain bg-[#E2CABE] w-[15vw] p-2 rounded-sm">
+<div className="carticon cursor-pointer object-contain bg-[#E2CABE] w-[15vw] portrait:sm:w-[10vw] p-2 rounded-sm">
 <PrismicNextImage  field={settings.data.carticon} />
 </div>
 
