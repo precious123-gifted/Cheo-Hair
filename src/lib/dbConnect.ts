499e66398f89bf84efba114ece8f7b3653e83978
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-05-08 14:55:36 +0100

    .env

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -10,7 +10,7 @@ export default async function dbConnect() {
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI!);
+    const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI!);
     connection.isConnected = db.connections[0].readyState;
 
     // Log successful connection message
