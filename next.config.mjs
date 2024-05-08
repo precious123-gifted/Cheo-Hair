a4386aa1574263034605e6ae45b13a776568306d
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-05-08 14:24:29 +0100

    .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    env:{
-      
-        NEXT_PUBLIC_VERCEL_URL:"https://cheo-hair-precious123gifteds-projects.vercel.app/",
-        MONGODB_URI:"mongodb+srv://preciousOG:preciousOG@cluster0.bfwetwr.mongodb.net/Products?retryWrites=true&w=majority&appName=Cluster0"
-
-    }
+ 
 
 
 };
