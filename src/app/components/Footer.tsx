32faa60a701ac8753bb33b7175223187a803915d
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-05-11 17:24:03 +0100

    Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,14 @@
 import { createClient } from "@/prismicio";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import Bounded from "./Bounded";
+import Image from "next/image";
+import gmailIcon from "../../../public/gmailIcon.png";
+import whatsappIcon from "../../../public/whatsappIcon.png";
+import instagramIcon from "../../../public/instagramIcon.png";
+import facebookIcon from "../../../public/facebookIcon.png";
+
+
+
 
 export default async function Footer() {
 
@@ -7,8 +17,60 @@ export default async function Footer() {
     const settings = await client.getSingle('app_settings')
 
   return (
-    <div>
-      this will be the footer
+    <Bounded>
+    <div className="content bg-[#14120F] text-[#DAD3D7] w-full px-5 py-8 pt-8 flex flex-row justify-between  portrait:sm:flex-row  portrait:sm:justify-between portrait:flex-col portrait:space-y-6 portrait:sm:space-y-0  ">
+
+
+<div className="addressNpolicySection space-y-3">
+
+<div className="addressDiv space-y-1">
+  <div className="title text-[1.7vw] portrait:sm:text-[4vw] portrait:text-[7vw]">Address</div>
+  <div className="address text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw]">{settings.data.address}</div>
+</div>
+
+<div className="refundPolicyLink text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw]"><PrismicNextLink field={settings.data.refundpolicy}>Link</PrismicNextLink></div>
+<div className="shippingPolicyLink text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw]"><PrismicNextLink field={settings.data.shippingpolicy}>Link</PrismicNextLink></div>
+<div className="termsNconditionLink text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw]"><PrismicNextLink field={settings.data.termsandconditions}>Link</PrismicNextLink></div>
+
+
+</div>
+
+
+
+
+<div className="customerSupportSection space-y-3">
+  <div className="header text-[1.7vw] portrait:sm:text-[4vw] portrait:text-[7vw]">Customer Support</div>
+
+  <div className="deliveryLink text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw]"><PrismicNextLink field={settings.data.shippingpolicy}>Link</PrismicNextLink></div>
+
+    <div className="contactDiv space-y-2">
+<div className="title text-[1.6vw] portrait:sm:text-[3.5vw] portrait:text-[6.5vw]">Contact Us</div>
+
+
+<div className="icons flex space-x-4"> 
+<Image src={ gmailIcon} alt="gmail icon" className=" w-[2vw] portrait:w-[5vw] portrait:sm:w-[3.5vw] object-contain"/>
+<Image src={ whatsappIcon} alt="whatsapp icon" className=" w-[2vw] portrait:w-[5vw] portrait:sm:w-[3.5vw] object-contain"/>
+<Image src={ instagramIcon} alt="instagram icon" className=" w-[2vw] portrait:w-[5vw] portrait:sm:w-[3.5vw] object-contain"/>
+<Image src={ facebookIcon} alt="facebook icon" className=" w-[2vw] portrait:w-[5vw] portrait:sm:w-[3.5vw] object-contain"/>
+
+</div>
+
+
+
+
+
+
     </div>
+
+</div>
+
+  
+    </div>
+  
+  <div className="copyRightDiv flex portrait:flex-col justify-between px-5 py-2 text-[#14120F] text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[3.7vw]  "> <div className="preciousOG">site by PreciousOG</div>   <div className="prodigital">Prodigital Company © 2024  - All Rights Reserved</div> </div>
+  
+  
+  
+      </Bounded>
   )
 }
