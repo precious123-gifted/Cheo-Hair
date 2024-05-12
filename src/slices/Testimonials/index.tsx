d1470fb59cc374e90704a39a8df47383f3b8d59e
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-05-12 16:18:55 +0100

    Testimonial-section[feat]:added swipping for courasel

--- src/slices/Testimonials/index.tsx
+++ src/slices/Testimonials/index.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import Bounded from "@/app/components/Bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
 import { Swiper,SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+
 
 
 /**
@@ -27,35 +34,42 @@ const Testimonials = ({ slice }: TestimonialsProps): JSX.Element => {
 
 <div className="header text-[2vw] portrait:sm:text-[4vw] portrait:text-[7vw] text-center">{slice.primary.heading}</div>
 
-<div className="testimonies text-[1.4vw] portrait:sm:text-[3vw] portrait:text-[5vw]  text-center leading-[2.5vw] portrait:leading-[8vw] portrait:sm:leading-[5vw]">
+
+
+  <div className="testimonies text-[1.4vw] portrait:sm:text-[3vw] portrait:text-[5vw]  text-center leading-[2.5vw] portrait:leading-[8vw] portrait:sm:leading-[5vw]">
 
 
 
+  <Swiper
+  navigation = {true}
+  pagination={{type:'fraction'}}
+  modules={[Navigation]}
+  className="w-[80vw]"
+  >
+      <div className="testifierContainer flex flex-col items-center space-y-[2.5vw] portrait:space-y-[4vw]">
+        {slice.items.map((item, index) => (
+          <SwiperSlide key={index} className="space-y-14">
+
+            
+            <div className="testifierTestimony flex items-center justify-center w-full ">  <div className="block   text-[1.6vw]  portrait:text-[5vw] portrait:sm:text-[3vw]">{item.testimony}</div></div>
+            
+            <div className="testifierName space-y-3 flex flex-col items-center justify-center text-[2vw]  portrait:text-[5vw] portrait:sm:text-[3.5vw]">
+            <PrismicNextImage field={item.testifierimage} className="w-[12vw] block  portrait:w-[26vw] portrait:sm:w-[23vw] object-contain rounded-full border-2 border-[#47412A]" />
+
+             <div className="block"> {item.testifiername}</div>  
+            </div>
 
 
-
-<div className="testifierContainer flex flex-col items-center space-y-[2.5vw] portrait:space-y-[4vw]">
-
-<div className="testifierTestimony">{
-  slice.items[0].testimony
-}</div>
-<div className="testifierImage">
- 
-  <PrismicNextImage field={slice.items[0].testifierimage} className="w-[12vw] portrait:w-[26vw] portrait:sm:w-[23vw] object-contain rounded-full border-2 border-[#47412A]"/>
+          </SwiperSlide>
+        ))}
+      </div>
+    </Swiper>
 
 </div>
-<div className="testifierName text-[2vw]  portrait:text-[5vw] portrait:sm:text-[5vw]">
-{
-  slice.items[0].testifiername
-}
-</div>
-
-</div>
 
 
 
 
-</div>
 
 </div>
 
