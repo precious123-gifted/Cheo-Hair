62cb0be3033d637a20cef5205cb95ae92c71c56f
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-05-15 00:14:32 +0100

    CartPage[feat]:restyled some texts

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -64,7 +64,7 @@ useEffect(() => {
         <th className="py-8">Total</th>
       </tr>
     </thead>
-    <tbody className="text-[1.2vw] portrait:text-[3vw] portrait:sm:text-[3vw] ">
+    <tbody className="text-[1.2vw] portrait:text-[3.6vw] portrait:sm:text-[3.2vw] ">
       {cartData.map((product) => (
         <tr key={product._id}  >
           <td className="flex portrait:flex-col items-start space-x-6 portrait:space-x-0 portrait:space-y-2">  
@@ -97,7 +97,7 @@ useEffect(() => {
 
   <div className="total w-full text-[1.3vw] portrait:text-[4.2vw] portrait:sm:text-[3.4vw] flex justify-between"><div className="text">Total</div><div className="amount">$2200</div></div>
 
-  <div className="checkoutBTN text-[1.6vw] portrait:text-[4.6vw] portrait:sm:text-[3.8vw] py-2 px-4 cursor-pointer text-[#E2CABE] bg-[#31503d] rounded-md">Checkout</div>
+  <div className="checkoutBTN w-full text-center text-[1.6vw] portrait:text-[5.8vw] portrait:sm:text-[4.6vw] py-2 px-4 cursor-pointer text-[#E2CABE] bg-[#31503d] rounded-md">Checkout</div>
 
 
 
