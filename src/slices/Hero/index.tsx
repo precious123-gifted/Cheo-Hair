a0c2babbf5df0287cc17c66e863ac8f90ae57fb6
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-05-19 15:33:16 +0100

    rechange

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -23,7 +23,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
      className="pt-[1.8vw] portrait:pt-[2vw] portrait:sm:pt-[3vw] pb-5 text-[#d3cbcf]    space-y-10  portrait:space-y-80 portrait:sm:space-y-40 " >
     <div className="content w-full py-1 px-1  flex space-x-[8vw] portrait:space-x-0 portrait:flex-col bg-[#141C1C] rounded-xl relative">
-<div className="heroimage  w-[22vw]  portrait:w-full    object-contain portrait:top-[46vw] portrait:left-0 portrait:bottom-0 portrait:m-auto portrait:sm:top-[unset] portrait:sm:left-[unset] portrait:sm:bottom-[unset] portrait:sm:m-[unset] right-0   portrait:sm:right-0"><Image className="landscape:rounded-l-xl portrait:rounded-t-xl portrait:h-[80vw] object-cover object-top "   alt="hero-image" src={heroImage} /></div>
+<div className="heroimage  w-[22vw]  portrait:w-full    object-contain portrait:top-[46vw] portrait:left-0 portrait:bottom-0 portrait:m-auto portrait:sm:top-[unset] portrait:sm:left-[unset] portrait:sm:bottom-[unset] portrait:sm:m-[unset] right-0   portrait:sm:right-0"><PrismicNextImage className="landscape:rounded-l-xl portrait:rounded-t-xl portrait:h-[80vw] object-cover object-top "   field={slice.primary.heroimage} /></div>
 
 <div className="headersection  bg-[#141C1C] h-fit  mt-5 flex flex-col items-start w-[50%] portrait:w-full portrait:sm:w-full   space-y-7 portrait:space-y-4">
 <div className="header1  text-[4vw] portrait:sm:text-[4vw] portrait:text-[6vw] leading-tight  tracking-free"><PrismicRichText field={slice.primary.header1}/></div>
--- src/slices/Whychooseus/index.tsx
+++ src/slices/Whychooseus/index.tsx
@@ -29,7 +29,7 @@ const Whychooseus = ({ slice }: WhychooseusProps): JSX.Element => {
 <div className="header text-[2vw] portrait:sm:text-[4vw] portrait:text-[7vw] text-center">{slice.primary.header}</div>
 
 <div className="content w-full py-1 px-1  flex space-x-[8vw] portrait:space-x-0 portrait:flex-col  rounded-xl relative">
-<div className="hairimage portrait:hidden w-[22vw]  portrait:w-full    object-contain portrait:top-[46vw] portrait:left-0 portrait:bottom-0 portrait:m-auto portrait:sm:top-[unset] portrait:sm:left-[unset] portrait:sm:bottom-[unset] portrait:sm:m-[unset] right-0   portrait:sm:right-0"><Image className="landscape:rounded-l-xl portrait:rounded-t-xl portrait:h-[80vw] object-cover object-top" src={whyImage} alt="whyus-image"/></div>
+<div className="hairimage portrait:hidden w-[22vw]  portrait:w-full    object-contain portrait:top-[46vw] portrait:left-0 portrait:bottom-0 portrait:m-auto portrait:sm:top-[unset] portrait:sm:left-[unset] portrait:sm:bottom-[unset] portrait:sm:m-[unset] right-0   portrait:sm:right-0"><PrismicNextImage className="landscape:rounded-l-xl portrait:rounded-t-xl portrait:h-[80vw] object-cover object-top" field={slice.primary.image} /></div>
 
 <div className="writeupsection   h-fit  mt-5 flex flex-col items-start w-[50%] portrait:w-full portrait:sm:w-full   space-y-10 portrait:space-y-8">
 <div className="writeup   text-[1.8vw] portrait:sm:text-[3vw] portrait:text-[6vw] leading-tight  tracking-free">{slice.primary.writeup}</div>
